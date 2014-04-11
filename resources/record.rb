47642a393bdae64a5d8339b21ea38ac70215a197
Peter Bell <peter_bell@htc.com>
2014-04-11 16:15:50 -0700

    added additional validation to route53_record attributes

--- resources/record.rb
+++ resources/record.rb
@@ -2,9 +2,9 @@ actions :create
 
 default_action :create
 
-attribute :name,                  :kind_of => String
+attribute :name,                  :kind_of => String , :required => true
 attribute :value,                 :kind_of => [ String, Array ]
-attribute :type,                  :kind_of => String
+attribute :type,                  :kind_of => String, :required => true , :equal_to => [ "A" , "AAAA" , "MX" , "SOA" , "SPF" , "CNAME" , "TXT" , "PTR" , "SRV" , "NS" ]
 attribute :ttl,                   :kind_of => Integer, :default => 3600
 attribute :zone_id,               :kind_of => String
 attribute :aws_access_key_id,     :kind_of => String
